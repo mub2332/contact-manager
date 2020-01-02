885f75f6a0f0e2a157f2d7bf5450967fa011db79
Mubtasim Mahmud <mubtasimmahmud20@gmail.com>
2020-01-02 11:36:14 +1100

    Updates

---
+++ precache-manifest.9ca5e7fca985dd4ee8b209f382348edc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fefa8e45eb657d409a6f3b281b66232",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "6d889fe357e97daf9aff",
+    "url": "/contact-manager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6a0a30e48b813e78d80d",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "6d889fe357e97daf9aff",
+    "url": "/contact-manager/static/js/2.11291bb3.chunk.js"
+  },
+  {
+    "revision": "d7c02b5cfd2fce7b07312cd42a7244b2",
+    "url": "/contact-manager/static/js/2.11291bb3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a0a30e48b813e78d80d",
+    "url": "/contact-manager/static/js/main.827a582a.chunk.js"
+  },
+  {
+    "revision": "7fc69bf43d6d11c29c63",
+    "url": "/contact-manager/static/js/runtime-main.eeb47f97.js"
+  }
+]);
